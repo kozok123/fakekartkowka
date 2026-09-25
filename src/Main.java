f59aa664cd239467d67b4460e35a231c955982fd
kozok123 <kozak02947@gmail.com>
2026-09-25 10:56:59 +0200

    gotowe

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //wylosuj 100 liczb z zakresu od 1 do 50
+
+        //zapisz liczby  w liście
+
+        //policz ile jest wartości unikatowych
+
+        //zapisz liczbe tylko z wartościami parzystymi
+
+        //oblicz ile jest liczb pierwszych wylosowanych
+
+
+        //zrob klase ksiazka z polem tytuł autor czy dostępna
+
+        //zrob klase czytelnik z polem imie i wyporzyczone ksiazki
+
+        //zrob klase biblioteka z polami ksiazki czytelnicy
+
+        //zaprogramuj wszystko co potrzebujesz zeby zrobic metode wypozycz
+        //wypozyczenie ksiazki polega na zmianie jej pola dostepne na niedostepne oraz czytelnik ma ta ksiazke w liscie ksiazek
+
+
+    }
+}
